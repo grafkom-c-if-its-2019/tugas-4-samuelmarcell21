a0a63bbeaef7c13a768d804b1394dd6501e6e8bb
samuelmarcell21 <samuelmarcell21@gmail.com>
2019-10-30 19:32:48 +0700

    Kubus

--- index.js
+++ index.js
@@ -5,28 +5,58 @@
 
   glUtils.SL.init({ callback:function() { main(); } });
 
-  function main() {
-    // Register Callbacks
+  // Register Callbacks
     // window.addEventListener('resize', resizer);
-
+    function main() {
     // Get canvas element and check if WebGL enabled
     var canvas = document.getElementById("glcanvas");
     var gl = glUtils.checkWebGL(canvas);
 
     // Initialize the shaders and program
-    var vertexShader = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v1.vertex);
     var fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v1.fragment);
-    var program = glUtils.createProgram(gl, vertexShader, fragmentShader);
+    // var program = glUtils.createProgram(gl, vertexShader, fragmentShader);
 
     var vertexShader2 = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v2.vertex);
+    var vertexShader3 = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v3.vertex);
     var program2 = glUtils.createProgram(gl, vertexShader2, fragmentShader);
+    var program3 = glUtils.createProgram(gl, vertexShader3, fragmentShader);
 
     canvas.addEventListener('mousedown', function(event) { onmousedown(event, points); });
-
 
     // resizer();
     var vertices4 = [];
     var vertices5 = [];
+
+    var kubus= ([
+      // Bawah
+      -0.3,  -0.8,  0.7,      255, 255, 255,          
+      0.4,  -0.8,  0.7,       255, 255, 255,          
+      0.4,  -0.8,  0.7,       255, 255, 255,          
+      0.4,  -0.8,  -0.6,      255, 255, 255,          
+      0.4,  -0.8,  -0.6,      255, 255, 255,          
+      -0.3,  -0.8,  -0.6,     255, 255, 255,          
+      -0.3,  -0.8,  -0.6,     255, 255, 255,          
+      -0.3,  -0.8,  0.7,      255, 255, 255,          
+      // Atas
+      -0.3,  0.6,  0.7,       255,255, 255,          
+      0.4,  0.6,  0.7,        255,255, 255,       
+      0.4,  0.6,  0.7,        255,255, 255,         
+      0.4,  0.6,  -0.6,      255,255, 255,          
+      0.4,  0.6,  -0.6,       255,255, 255,          
+      -0.3,  0.6,  -0.6,      255,255, 255,          
+      -0.3,  0.6,  -0.6,      255,255, 255,          
+      -0.3,  0.6,  0.7,       255,255, 255,          
+      // Belakang
+      -0.3,  -0.8,  0.7,      255,255, 255,            
+      -0.3,  0.6,  0.7,       255,255, 255,           
+      0.4,  -0.8,  0.7,      255,255, 255,            
+      0.4,  0.6,  0.7,        255,255, 255,            
+      // Depan
+      0.4,  -0.8,  -0.6,      255,255, 255,            
+      0.4,  0.6,  -0.6,       255,255, 255,           
+      -0.3,  -0.8,  -0.6,     255,255, 255,            
+      -0.3,  0.6,  -0.6,      255,255, 255      
+    ]);
 
     for (var i=-180; i<=90; i+=1) {
       // degrees to radians
@@ -65,36 +95,6 @@
       vertices5 = vertices5.concat(vert1);
       vertices5 = vertices5.concat(vert2);
     }
- 
-    function drawShapes(type,vertices,n){
-      var triangleVertexBufferObject = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
-  
-      var vPosition = gl.getAttribLocation(program, 'vPosition');
-      var vColor = gl.getAttribLocation(program, 'vColor');
-    
-      gl.vertexAttribPointer(
-        vPosition,  // variabel yang memegang posisi attribute di shader
-        2,          // jumlah elemen per atribut
-        gl.FLOAT,   // tipe data atribut
-        gl.FALSE, 
-        5 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap vertex 
-        0                                   // offset dari posisi elemen di array
-      );
-      gl.vertexAttribPointer(
-        vColor,
-        3,
-        gl.FLOAT,
-        gl.FALSE,
-        5 * Float32Array.BYTES_PER_ELEMENT,
-        2 * Float32Array.BYTES_PER_ELEMENT
-      );
-      gl.enableVertexAttribArray(vPosition);
-      gl.enableVertexAttribArray(vColor);
-
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-      gl.drawArrays(type, 0, n);
-    }  
 
     function drawShapes2(type,vertices,n){
       var triangleVertexBufferObject = gl.createBuffer();
@@ -155,6 +155,52 @@
   // render();
   render2();
 
+  function drawShapesKubus(type,vertices,n) {
+    var vertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferObject);
+  
+    var vPosition = gl.getAttribLocation(program3, 'vPosition');
+    var vColor = gl.getAttribLocation(program3, 'vColor');
+  
+    gl.vertexAttribPointer(
+      vPosition, //variabel pemegang posisi atribut di shader
+      3,          // jumlah elemen per atribut
+      gl.FLOAT,   // tipe data atribut
+      gl.FALSE,   
+      6 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap vertex
+      0
+    );
+  
+    gl.vertexAttribPointer(
+      vColor, 
+      3, 
+      gl.FLOAT, 
+      gl.FALSE, 
+      6 * Float32Array.BYTES_PER_ELEMENT, 
+      3 * Float32Array.BYTES_PER_ELEMENT
+    );
+    gl.enableVertexAttribArray(vPosition);
+    gl.enableVertexAttribArray(vColor);
+  
+    var vPosition = gl.getAttribLocation(program3, 'vPosition');
+    var vColor = gl.getAttribLocation(program3, 'vColor');
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.drawArrays(type, 0, n);
+  }
+
+  var thetaLocCube = gl.getUniformLocation(program3, 'theta');
+  
+  function renderKubus()
+  {
+    gl.useProgram(program3);
+    var thetaCube = [10, 10, 0];
+    gl.uniform3fv(thetaLocCube, thetaCube);
+    drawShapesKubus(gl.LINES, kubus , 24);
+    requestAnimationFrame(renderKubus);
+  }
+
+  renderKubus();
+
   function onmousedown(event, points) {
 
     // find x and y of click
@@ -176,8 +222,6 @@
 
     console.log(x + "  " + y);
   }
-
 }
-
 
 })(window || this);
